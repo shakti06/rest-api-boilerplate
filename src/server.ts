ec7b558a455747c6b6002bb667b4e1c0d9f1da2a
shakti06 <shaktibhujel06@gmail.com>
2023-09-08 15:14:01 +1000

    categories and video migrations added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import "reflect-metadata";
-import express from "express";
+import express from 'express';
+import { AppDataSource } from './data-source';
 
-const app = express();
-
-app.listen(3000, ()=> console.log('app is running'));
+AppDataSource.initialize().then(() => {
+   const app = express();
+   app.use(express.json())
+   app.get('/', (req, res) => {
+   return res.json('Established connection!');
+ })
+  return app.listen(3000, () => console.log("app is running"));
+})
